f3fd213ba432d2d9722ee4fc3344636bffa21696
Robert Laws <roblaws@me.com>
2019-05-21 08:28:23 +0300

    begin controllers and crud section

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,8 +1,10 @@
 class AuthorsController < ApplicationController
   def index
+    @authors = Author.sorted
   end
 
   def show
+    @author = Author.find(params[:id])
   end
 
   def new
--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,5 @@ class Author < ApplicationRecord
   has_many :books
 
   scope :get_author_by_id, ->(id) { where("id = ?", id) }
+  scope :sorted, -> { order(name: :asc) }
 end
